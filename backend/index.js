004a7efc2c1ae59b021fbbfd02d1745ba1e5c1bc
Sahil Upadhyay <67515591+Sahil-Upadhyay@users.noreply.github.com>
2024-05-11 08:52:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://prismatic-daffodil-ab3e24.netlify.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
