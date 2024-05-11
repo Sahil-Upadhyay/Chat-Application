b8c89467ab419f8c0a25bd59aba7cef8d2a0ed6f
Sahil Upadhyay <67515591+Sahil-Upadhyay@users.noreply.github.com>
2024-05-11 09:08:10 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000'],
+        origin:['https://prismatic-daffodil-ab3e24.netlify.app'],
         methods:['GET', 'POST'],
     },
 });
